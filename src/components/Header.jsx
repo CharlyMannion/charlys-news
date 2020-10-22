b17fc8a2fe37bd8896d2857ecf74dd4670094c09
CharlyMannion <charlottemannion100@gmail.com>
2020-10-22 12:36:35 +0100

    adds welcome message to the user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from '@reach/router';
+import {loggedInUserName} from '../utils/constants'
 
 const Header = () => {
     return (
     <header className="header">
         <Link to='/'><h1>Northcoders News</h1></Link>
+        <h3>Welcome {loggedInUserName}</h3>
     </header>
     )
 }
